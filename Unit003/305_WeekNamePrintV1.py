494cb181fb2617661779afc5f303e674f7171632
Ted Liu <shb.liu@outlook.com>
2020-03-09 09:12:12 +0800

    20200309

---
+++ Unit003/305_WeekNamePrintV1.py
@@ -0,0 +1,5 @@
+#305_WeekNamePrint.py
+weekStr = '星期一星期二星期三星期四星期五星期六星期日'
+weekID = eval(input('请输入星期数字（1-7）：'))
+pos = (weekID - 1) * 3
+print(weekStr[pos: pos + 3])    #切片用中括号和冒号
---
+++ Unit003/306_WeekNamePrintV2.py
@@ -0,0 +1,4 @@
+#306_WeekNamePrintV2.py
+weekStr = '一二三四五六日'
+weekID = eval(input('请输入星期数字（1-7）：'))
+print('星期' + weekStr[weekID - 1])
