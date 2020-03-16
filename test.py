ed767743349f85a32ed304aff9605e3fec0d2e7a
mzm-l <tefodliu@gmail.com>
2020-03-16 21:39:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,14 @@
-import turtle as t
-for i in range(25):
-    t.fd(2*i)
-    t.lt(90)
-t.done()
+t = input()
+ori = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+cod = "DEFGHIJKLMNOPQRSTUVWXYZABC"
+oriL = ori.lower()
+codL = cod.lower()
+nT = ""
+for i in t:
+    if i in ori:
+        nT += cod[ori.find(i)]
+    elif i in oriL:
+        nT += cod.lower()[ori.lower().find(i)]
+    else:
+        nT += i
+print(nT)
