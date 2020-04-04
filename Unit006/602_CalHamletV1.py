303d03e82d2def9378626ec541e227565d9b9044
shb-liu <tefodliu@gmail.com>
2020-04-05 03:34:57 +0800

    Unit006/602

---
+++ Unit006/602_CalHamletV1.py
@@ -0,0 +1,18 @@
+#602_CalHamletV1.py
+def getText():
+    text = open("hamlet.txt", "r").read()
+    text = text.lower()
+    for ch in '!"#$%&()*+,-./:;<=>?@[\\]^_{|}~`\'':
+        text = text.replace("ch", " ")
+    return text
+
+hamletText = getText()
+words = hamletText.split()
+counts = {}
+for word in words:
+    counts[word] = counts.get(word,0) + 1
+items = list(counts.items())
+items.sort(key=lambda x:x[1], reverse=True)
+for i in range(10):
+    word, count = items[i]
+    print("{0:<10}{1:>5}".format(word, count))
