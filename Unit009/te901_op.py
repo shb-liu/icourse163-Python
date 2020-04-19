d4a68f9eaafa90c8880bb9e7d3861e6c1af38a1a
shb-liu <tefodliu@gmail.com>
2020-04-19 22:03:42 +0800

    Unit009

---
+++ Unit009/te901_op.py
@@ -0,0 +1,3 @@
+#te901_op.py
+import sys
+print("RECLIMIT:{}, EXEPATH:{}, UNICODE:{}".format(sys.getrecursionlimit(), sys.executable, sys.maxunicode))
---
+++ Unit009/te902_table.py
@@ -0,0 +1,9 @@
+#te902_table.py
+from tabulate import tabulate
+data = [ ["北京理工大学", "985", 2000], \
+         ["清华大学", "985", 3000], \
+         ["大连理工大学", "985", 4000], \
+         ["深圳大学", "211", 2000], \
+         ["沈阳大学", "省本", 2000], \
+    ]
+print(tabulate(data, tablefmt="grid"))
